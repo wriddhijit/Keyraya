201a940ba4b1609dd14ad17f37b4fbe2b6aa23be
wriddhijit <wriddhijitroy@gmail.com>
2024-04-12 10:18:39 +0530

    Privacy Policy

--- src/pages/Privacy_Policy.jsx
+++ src/pages/Privacy_Policy.jsx
@@ -1,7 +1,101 @@
 function Privacypolicy() {
   return (
     <>
-      <div>Privacy Policy</div>
+      <div className="my-10 mt-14">
+        <p className=" text-4xl mb-16 text-center tracking-wide font-semibold">
+          Privacy Policy
+        </p>
+        <p className=" px-24 mt-8 mb-8 text-2xl text-left font-normal leading-relaxed tracking-wide ">
+          This Privacy Policy constitutes an electronic record serving as an
+          electronic contract established under the Information Technology Act,
+          2000, along with the associated regulations and amended provisions
+          concerning electronic documents/records within various statutes, as
+          altered by the Information Technology Act, 2000, or any relevant
+          statute or regulation under any applicable jurisdiction. It is to be
+          noted that this Privacy Policy does not necessitate any physical,
+          electronic, or digital signature.{" "}
+        </p>
+        <p className=" px-24 mt-8 mb-8 text-2xl text-left font-normal leading-relaxed tracking-wide ">
+          The objective of this Privacy Policy is to establish a comprehensive
+          framework for the collection, utilization, and safeguarding of any
+          personal and/or sensitive data obtained by us. It delineates our
+          protocols for the collection, utilization, processing, disclosure, and
+          protection of any information obtained through the Platform. Any terms
+          capitalized in this Privacy Policy, not defined herein, shall carry
+          the same meaning ascribed to them in our Terms of Service. Any
+          reference to the Privacy Policy in this document shall imply and
+          pertain to the latest version of the Privacy Policy.
+        </p>
+        <p className=" px-24 mt-8 mb-8 text-3xl text-left font-semibold tracking-normal ">
+          Consent:
+        </p>
+        <p className=" px-24 mt-8 mb-8 text-xl text-left font-normal leading-relaxed tracking-wide ">
+          This Privacy Policy constitutes a legally binding agreement between
+          you and the Company. The terms delineated herein shall become
+          effective upon your acceptance of the same or through your utilization
+          of our services, thereby governing the relationship between the
+          Company and you concerning your use of the Platform and our services.
+        </p>
+        <p className=" px-24 mt-8 mb-8 text-xl text-left font-normal leading-relaxed tracking-wide ">
+          By utilizing this Platform and availing our services, you signify that
+          you comprehend, agree to, and consent to this Privacy Policy. In the
+          event that you disagree with the terms of this Privacy Policy, kindly
+          refrain from using this website or availing our services.
+        </p>
+        <p className=" px-24 mt-8 mb-8 text-xl text-left font-normal leading-relaxed tracking-wide ">
+          Please note that any Information acquired by us shall be:
+        </p>
+        <p className=" px-24 pl-32 mt-8 mb-8 text-xl text-left font-normal leading-relaxed tracking-wide ">
+          <ul className=" list-disc">
+            <li> Processed fairly and lawfully for rendering the Services;</li>
+            <li> Obtained solely for specified and lawful purposes;</li>
+            <li>
+              {" "}
+              Adequate, relevant, and not excessive concerning the purpose for
+              which it is required;
+            </li>
+            <li>
+              {" "}
+              Able to be reviewed by the User periodically and updated as
+              necessary;
+            </li>
+            <li>
+              {" "}
+              Not retained longer than necessary for the intended purpose or as
+              mandated by applicable law.
+            </li>
+          </ul>
+        </p>
+        <p className=" px-24 mt-8 mb-8 text-xl text-left font-normal leading-relaxed tracking-wide ">
+          Should you disagree with this Privacy Policy, you reserve the right to
+          refuse or withdraw your consent at any time. Alternatively, you may
+          choose not to provide us with any Personal Information. It is
+          understood that under such circumstances, we may be unable to render
+          Services. Any such notification to withdraw your consent can be
+          directed to keyraya.info@gmail.com.
+        </p>
+        <p className=" px-24 mt-8 mb-8 text-3xl text-left font-semibold tracking-normal ">
+          Liability Disclaimer:
+        </p>
+      </div>
+      <p className=" px-24 mt-8 mb-8 text-xl text-left font-normal leading-relaxed tracking-wide ">
+        We shall not be held liable for any loss or damage sustained due to any
+        disclosure (whether inadvertent or otherwise) of any data, if such
+        disclosure:
+        <br></br>
+        <span className=" font-semibold">
+          (A) Is necessary for sharing your information for legitimate purposes;
+        </span>{" "}
+        or
+        <br></br>
+        <span className=" font-semibold">
+          (B) Occurs through no fault, act, or omission of the Company.
+        </span>
+      </p>
+      <p className="mt-6 mb-10 text-center text-2xl font-medium tracking-wide">
+        For the full privacy policy, visit our website or contact our support
+        team.
+      </p>
     </>
   );
 }
