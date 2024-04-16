95796f793660198bb942bd0ec392bd2fdba49dea
wriddhijit <wriddhijitroy@gmail.com>
2024-04-16 21:20:29 +0530

    changes

--- backend/seeds/seedMotorcycles.js
+++ backend/seeds/seedMotorcycles.js
@@ -11,25 +11,49 @@ mongoose.connect(process.env.MONGO_URI, {
 
 const motorcycles = [
   {
-    title:'Kawasaki Ninja 40',
+    title:'Kawasaki Ninja H2',
     model: '',
-    price: 4999,
+    price: 900,
     description: 'Lightweight, sharp-looking, high-performance sport model',
     image: '/seeds/bikepic/aprilia.jpg', 
     inStock: true
   },
   {
-    title:'Harley Davidson Street 750',
+    title:'BMW S1000RR',
     model: '',
-    price: 7500,
+    price: 700,
     description: 'Urban cruiser with authentic Harley-Davidson style',
     image: 'http://example.com/path/to/another-image.jpg',
     inStock: false
   },
   {
-    title:'Harley Davidson Street 750',
+    title:'Ducati Panigale V4',
     model: '',
-    price: 7500,
+    price: 800,
+    description: 'Urban cruiser with authentic Harley-Davidson style',
+    image: 'http://example.com/path/to/another-image.jpg',
+    inStock: false
+  },
+  {
+    title:'Triumph Street Triple 765RS',
+    model: '',
+    price: 500,
+    description: 'Urban cruiser with authentic Harley-Davidson style',
+    image: 'http://example.com/path/to/another-image.jpg',
+    inStock: false
+  },
+  {
+    title:'Yamaha YZF R1',
+    model: '',
+    price: 750,
+    description: 'Urban cruiser with authentic Harley-Davidson style',
+    image: 'http://example.com/path/to/another-image.jpg',
+    inStock: false
+  },
+  {
+    title:'Aprilia RSV4',
+    model: '',
+    price: 650,
     description: 'Urban cruiser with authentic Harley-Davidson style',
     image: 'http://example.com/path/to/another-image.jpg',
     inStock: false
